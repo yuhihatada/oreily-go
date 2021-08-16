0184098f059c25185d0dc90294b78ba636dc4f67
畑田祐陽 <hatadayuuhi@hatadayuuhinoMacBook-Pro.local>
2021-08-17 08:36:27 +0900

    P28一時コミット

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -18,16 +19,20 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
 	r := newRoom()
+	//r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// チャットルームの開始
 	go r.run()
 	// Webサーバーの起動
+	log.Println("Webサーバーを開始します。ポート : ", *addr)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
--- chat/room.go
+++ chat/room.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"oreily-go/trace"
 
 	"github.com/gorilla/websocket"
 )
@@ -12,6 +13,7 @@ type room struct {
 	join    chan *client
 	leave   chan *client
 	clients map[*client]bool
+	tracer  trace.Tracer
 }
 
 func newRoom() *room {
@@ -29,20 +31,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			// 参加
 			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			// 退室
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
 		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを受信しました : ", string(msg))
 			// 全てのクライアントにメッセージを送信
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
-					//send a message
+					//メッセージを送信
+					r.tracer.Trace("-- クライアントに送信されました")
 				default:
 					// 送信に失敗
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace("-- 送信に失敗しました。クライアントをクリーンアップします")
 				}
 			}
 		}
